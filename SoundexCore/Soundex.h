26d5069f5ac1e3c4a74caea1f1eac4d35b35f044
yutong pang <wstcpyt@gmail.com>
2015-06-22 15:55:09 -0400

    c2_32

--- SoundexCore/Soundex.h
+++ SoundexCore/Soundex.h
@@ -7,6 +7,7 @@
 
 #include <string>
 #include <unordered_map>
+#include <map>
 
 class Soundex
 {
@@ -14,27 +15,51 @@ public:
     static const size_t MaxCodeLength = 4;
     std::string encode(const std::string &word) const
     {
-        return zeroPad(head(word) + encodedDigits(word));
+        return zeroPad(head(word) + encodedDigits(tail(word)));
+    }
+    std::string encodedDigit(char letter) const {
+        const std::unordered_map<char,std::string> encodings{
+                {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
+                {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"},
+                {'s', "2"}, {'x', "2"}, {'z', "2"},
+                {'d', "3"}, {'t', "3"},
+                {'l', "4"},
+                {'m', "5"}, {'n', "5"},
+                {'r', "6"}
+        };
+        auto it = encodings.find(letter);
+        return it == encodings.end() ? "": it-> second;
     }
 
 private:
-
     std::string head(const std::string& word) const{
         return word.substr(0, 1);
     }
-    std::string encodedDigits(const std::string& word) const {
-        if (word.length() > 1) return encodedDigit(word[1]);
-        return "";
+
+    std::string tail(const std::string& word) const {
+        return word.substr(1);
     }
 
-    std::string encodedDigit(char letter) const {
-        const std::unordered_map<char,std::string> encodings {
-                {'b', "1"},
-                {'c', "2"},
-                {'d', "3"}
-        };
-        return encodings.find(letter) -> second;
+    std::string encodedDigits(const std::string& word) const {
+        std::string encoding;
+        for (auto letter: word)
+        {
+            if (isComplete(encoding)) break;
+            if (encodedDigit(letter) != lastDigit(encoding))
+            encoding += encodedDigit(letter);
+        }
+        return encoding;
     }
+
+    std::string lastDigit(const std::string& encoding) const {
+        if(encoding.empty()) return "";
+        return std::string(1, encoding.back());
+    }
+
+    bool isComplete (const std::string& encoding) const {
+        return encoding.length() == MaxCodeLength - 1;
+    }
+
     std::string zeroPad(const std::string& word) const{
         auto zeroNeeded = MaxCodeLength - word.length();
         return word + std::string(zeroNeeded, '0');
--- SoundexCore/unit_test/SoundexTest.cpp
+++ SoundexCore/unit_test/SoundexTest.cpp
@@ -20,7 +20,28 @@ TEST_F(SoundexEnxoding, PadsWithZerosToEnsureThreeDigits)
 }
 
 TEST_F(SoundexEnxoding, ReplacesConsonantsWithAppropriateDigits){
-    EXPECT_THAT(soundex.encode("Ab"), Eq("A100"));
-    EXPECT_THAT(soundex.encode("Ac"), Eq("A200"));
-    EXPECT_THAT(soundex.encode("Ad"), Eq("A300"));
+    ASSERT_THAT(soundex.encode("Ax"), Eq("A200"));
+}
+
+TEST_F(SoundexEnxoding, IgnoresNonAlphabetics) {
+    ASSERT_THAT(soundex.encode("A#"), Eq("A000"));
+}
+
+TEST_F(SoundexEnxoding, ReplacesMultipleConsonantsWithDigits) {
+    ASSERT_THAT(soundex.encode("Acdl"), Eq("A234"));
+}
+
+TEST_F(SoundexEnxoding, LimitsLengthToFourCharacters) {
+    ASSERT_THAT(soundex.encode("Dcdlb").length(), Eq(4u));
+}
+
+TEST_F(SoundexEnxoding, IgnoresVowelLikeLetters) {
+    ASSERT_THAT(soundex.encode("Baeiouhycdl"), Eq("B234"));
+}
+
+TEST_F(SoundexEnxoding, CombinesDuplicateEncodings) {
+    ASSERT_THAT(soundex.encodedDigit('b'), Eq(soundex.encodedDigit('f')));
+    ASSERT_THAT(soundex.encodedDigit('c'), Eq(soundex.encodedDigit('g')));
+    ASSERT_THAT(soundex.encodedDigit('d'), Eq(soundex.encodedDigit('t')));
+    ASSERT_THAT(soundex.encode("Abfcgdt"), Eq("A123"));
 }
